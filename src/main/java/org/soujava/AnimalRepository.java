a46bc063165c28662d0df3ea9227151e0aaf5953
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-15 10:38:10 +0000

    Add an annotation to AnimalRepository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/AnimalRepository.java
+++ src/main/java/org/soujava/AnimalRepository.java
@@ -2,8 +2,10 @@ package org.soujava;
 
 import jakarta.nosql.mapping.Repository;
 
+import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
+@ApplicationScoped
 public interface AnimalRepository extends Repository<Animal, String> {
     List<Animal> findAll();
 }
