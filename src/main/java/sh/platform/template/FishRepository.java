fe7d00c64020ca2489bb798b5af8af6056ab5dfb
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-15 07:23:31 +0000

    Define fish repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/sh/platform/template/FishRepository.java
+++ src/main/java/sh/platform/template/FishRepository.java
@@ -1,2 +1,9 @@
-package sh.platform.template;public class FishRepository {
+package sh.platform.template;
+
+import jakarta.nosql.mapping.Repository;
+
+import java.util.List;
+
+public interface FishRepository extends Repository<Fish, String> {
+    List<Fish> findAll();
 }
